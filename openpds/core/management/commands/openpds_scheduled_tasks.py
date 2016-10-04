16e60b826efcd4168a0198a103f5af6d7fda0a30
Eric Schlossberg <eric.schlossberg@gmail.com>
2016-10-04 19:40:24 -0400

    celery

---
+++ openpds/core/management/commands/openpds_scheduled_tasks.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+import logging
+from openpds.questions.socialhealth_tasks import recentSocialHealthScores
+from openpds.questions.tasks import ensureFunfIndexes, recentProbeCounts, dumpFunfData, dumpSurveyData
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        recentProbeCounts.delay()
+        recentSocialHealthScores.delay()
+        ensureFunfIndexes.delay()
+        dumpFunfData.delay()
+        dumpSurveyData.delay()
