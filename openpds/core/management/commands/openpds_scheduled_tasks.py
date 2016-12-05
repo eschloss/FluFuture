7dc94a313997c3baccd50f78a257828ebbd8c987
Eric Schlossberg <orpheuskl@gmail.com>
2016-12-05 14:18:08 -0500

    cleanup

--- openpds/core/management/commands/openpds_scheduled_tasks.py
+++ openpds/core/management/commands/openpds_scheduled_tasks.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 import logging
 from openpds.questions.socialhealth_tasks import recentSocialHealthScores
-from openpds.questions.tasks import ensureFunfIndexes, recentProbeCounts, dumpFunfData, dumpSurveyData, flumojiNotifications, emojiLocations, deleteUnusedProfiles, profileLocations, setInfluenceScores
+from openpds.questions.tasks import ensureFunfIndexes, recentProbeCounts, dumpFunfData, dumpSurveyData, flumojiNotifications, emojiLocations, deleteUnusedProfiles, profileLocations, setInfluenceScores, cleanExpiredReferrals
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
